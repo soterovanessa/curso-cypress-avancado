59aa78053960f0432c211810717c7947a87b00f4
Vanessa Sotero <soterovanessa@gmail.com>
2024-11-06 11:24:03 -0300

    feat: Teste simulando um atraso na API

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -273,3 +273,19 @@ context("Errors", () => {
     cy.get("p:contains(Something went wrong ...)").should("be.visible");
   });
 });
+
+// Teste simulando um atraso na API
+// Validar o que o "loading é apresentando quando há um atraso na API"
+it.only('shows a "Loading ..." state before showing the results', () => {
+  cy.intercept("GET", "**/search**", {
+    delay: 1000,
+    fixture: "stories",
+  }).as("getDelayedStories");
+
+  cy.visit("/");
+
+  cy.assertLoadingIsShownAndHidden();
+  cy.wait("@getDelayedStories");
+
+  cy.get(".item").should("have.length", 2);
+});
